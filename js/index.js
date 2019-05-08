a09f87e4990b255bba98c41cc735aee703a3f662
BarryDong <286084845@qq.com>
2019-05-08 08:42:58 +0800

    随机出现灰太狼和小灰灰以及随机出现的位置

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ $('.rules').on('click', function () {
 $('.start').on('click', function () {
   $(this).stop().fadeOut(100)
   progressHandler()
+  wolfAnimation()
 })
 
 $('.mask > button').on('click', function () {
@@ -35,4 +36,32 @@ function progressHandler() {
       clearInterval(timer)
     }
   }, 1000)
+}
+
+// 处理灰太狼动画
+function wolfAnimation() {
+  var wolf1 = ['./img/h0.png', './img/h1.png', './img/h2.png', './img/h3.png', './img/h4.png', './img/h5.png', './img/h6.png', './img/h7.png', './img/h8.png', './img/h9.png']
+  var wolf2 = ['./img/x0.png', './img/x1.png', './img/x2.png', './img/x3.png', './img/x4.png', './img/x5.png', './img/x6.png', './img/x7.png', './img/x8.png', './img/x9.png']
+  var arrPos = [
+    {left: '100px', top: '115px'},
+    {left: '20px', top: '160px'},
+    {left: '190px', top: '142px'},
+    {left: '105px', top: '193px'},
+    {left: '19px', top: '221px'},
+    {left: '202px', top: '212px'},
+    {left: '120px', top: '275px'},
+    {left: '30px', top: '295px'},
+    {left: '209px', top: '297px'},
+  ]
+  var $img = $('<img src="">')
+  var positionIndex = Math.floor(  Math.random() * 8)
+  var wolfArrRandom = Math.round(Math.random())
+  var wolfType = wolfArrRandom === 0 ? wolf1 : wolf2
+  $img.css({
+    position: 'absolute',
+    left: arrPos[positionIndex].left,
+    top: arrPos[positionIndex].top
+  })
+  $img.attr('src', wolfType[0])
+  $('.container').append($img)
 }
